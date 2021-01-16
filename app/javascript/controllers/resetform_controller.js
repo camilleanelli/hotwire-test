5e2653d0bbea80d5ba184e611f6cd42e524dc9f6
camille anelli <camilleanelli32@gmail.com>
2021-01-16 15:19:53 +0100

    Fix stimulus

--- app/javascript/controllers/resetform_controller.js
+++ app/javascript/controllers/resetform_controller.js
@@ -3,7 +3,7 @@ import { Controller } from 'stimulus'
 export default class extends Controller {
   static targets = ["button"]
    // on cree une fonction reset pour que le bouton soit à nouveau disponible apres le click
-  connect() {
+  reset() {
     console.log('ok')
     this.buttonTarget.disabled = false
   }
